6b79c1b8f775cafbc84535a1a86913fc0ae301d2
Scott Hardie <scottrmhardie@gmail.com>
2026-05-23 14:43:54 -0400

    chore: uncomment public_docs_features in sphinx config (#160)
    * chore: uncomment public_docs_features in sphinx config
    
    * chore: uncomment public_docs_features in sphinx config and fix CI

--- src/lib/gateway/server.ts
+++ src/lib/gateway/server.ts
@@ -62,8 +62,9 @@ export class ApiGateway {
         }
         return;
       }
-
-      if (req.url === "/grid/register" && req.method === "POST") {
+    }
+    if (req.method === "POST") {
+      if (req.url === "/grid/register") {
         let body = "";
         req.on("data", chunk => { body += chunk.toString(); });
         req.on("end", () => {
@@ -81,7 +82,7 @@ export class ApiGateway {
         return;
       }
 
-      if (req.url === "/grid/workload" && req.method === "POST") {
+      if (req.url === "/grid/workload") {
         let body = "";
         req.on("data", chunk => { body += chunk.toString(); });
         req.on("end", () => {
